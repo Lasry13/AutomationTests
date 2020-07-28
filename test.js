b9fada31a48669272be81098b4fa39d61299dcc4
danie <daniel.lasry9@gmail.com>
2020-07-28 08:57:24 +0300

    Initial project and add Array tests

---
+++ test.js
@@ -0,0 +1,33 @@
+const expect = require('chai').expect;
+
+const arrayMap = (arr) => {
+    return arr.map(a => a + 2)
+}
+
+const arrayFilter = (arr) => {
+    return arr.filter(a => a % 2 === 0)
+}
+
+const arraySum = (arr) => {
+    return arr.reduce((a, b) => a + b)
+}
+
+describe('Array', () => {
+    describe('#testArrayMap', () => {
+        it('Should return the sum of all elements in the array', () => {
+            console.log(arrayMap([3, 5, 2]))
+        });
+    });
+
+    describe('#testArrayFilter', () => {
+        it('Should return the sum of all elements in the array', () => {
+            console.log(arrayFilter([3, 5, 2]))
+        });
+    });
+
+    describe('#testArraySum', () => {
+        it('Should return the sum of all elements in the array', () => {
+            expect(arraySum([4, 3, 5, 2])).to.equal(14)
+        });
+    });
+});
