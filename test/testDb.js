dc19566c666228e63f3b8a2350d84e175ef91ab4
danie <daniel.lasry9@gmail.com>
2020-07-29 21:27:34 +0300

    Add Test suite for DB reading

--- test/testDb.js
+++ test/testDb.js
@@ -2,23 +2,34 @@ const expect = require('chai').expect;
 const db = require('../db');
 const {Pool} = require('pg')
 const config = require('../config.json')
-const pool = new Pool(config.db)
 const logging = require('../logging')
 const logger = logging.logger
 
-describe('select', () => {
-    describe('#getByProductId', () => {
-        it('responds with matching records', async () => {
-            let product = await db.queryDb(pool, [1])
-            console.log(product)
+describe('Test CRUD Operations on DB', () => {
+    describe('#Read', () => {
+        const pool = new Pool(config.db)
+        let product;
+        before(async () => {
+            try {
+                product = await db.queryDb(pool, [1])
+            } catch (e) {
+                logger.error(e)
+            }
+        });
+        it('Should return product id', async () => {
+            expect(product[0].id).to.equal(1)
+        });
+
+        it('Should return product name', async () => {
+            expect(product[0].name).to.equal('Eggs')
+        });
+
+        it('Should return product code', async () => {
             expect(product.length).to.equal(1)
         });
 
-        it('responds with matching records', async () => {
-            let product = await db.queryDb(pool, [2])
-            console.log(product)
-            logger.info(product)
-            expect(product[0].id).to.equal(2)
+        after(() => {
+            pool.end()
         });
     });
 });
